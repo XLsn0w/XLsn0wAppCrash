c45000d49d8a2a4562b7e00479f737a2274952a1
XLsn0w <XLsn0w@outlook.com>
2018-06-02 17:15:17 +0800

    init

---
+++ XLsn0wAppCrash/Swift4.swift
@@ -0,0 +1,13 @@
+//
+//  Swift4.swift
+//  XLsn0wAppCrash
+//
+//  Created by XLsn0w on 2018/6/2.
+//  Copyright © 2018年 XLsn0w. All rights reserved.
+//
+
+import UIKit
+
+class Swift4: NSObject {
+
+}
---
+++ XLsn0wAppCrash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XLsn0wAppCrash
+//
+//  Created by XLsn0w on 2018/6/2.
+//  Copyright © 2018年 XLsn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
