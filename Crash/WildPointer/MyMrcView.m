784f8a36708cf4d43b9a23f86fa5b811e22b86ff
xlsn0w <xlsn0wios@gmail.com>
2018-06-30 13:47:28 +0800

    add

--- Crash/WildPointer/MyMrcView.m
+++ Crash/WildPointer/MyMrcView.m
@@ -15,12 +15,12 @@
     self = [super init];
     if (self) {
         UIView *tempView = [[UIView alloc]init];
-        [tempView release];
+//        [tempView release];
         
         //AppDelegate里开启了init_safe_free()，表示释放的内存不会被覆盖，所以tempView调用setNeedsDisplay肯定会崩溃
         //如果不开启init_safe_free()，下面创建了10个UIView，那块被释放的内存可能会被覆盖，所以tempView调用setNeedsDisplay不一定会崩溃
         for (int i = 0; i < 10; i ++) {
-            UIView *temp = [[[UIView alloc]init] autorelease];
+            UIView *temp = [[UIView alloc]init];
             [self addSubview:temp];
         }
         [tempView setNeedsDisplay];
---
+++ XLsn0wAppCrash/Swift 4.2.swift
@@ -0,0 +1,9 @@
+//
+//  Swift 4.2.swift
+//  XLsn0wAppCrash
+//
+//  Created by HL on 2018/6/29.
+//  Copyright © 2018年 XLsn0w. All rights reserved.
+//
+
+import Foundation
